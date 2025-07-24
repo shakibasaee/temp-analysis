45e7ac919e6a17bbfad247fb73e47795b4afe183
Kasra Naal <kasranaalagsp1088@gamil.com>
2025-07-24 16:41:05 +0330

    clean data and show summry of statistics

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import pandas as pd
 
-df = pd.read_csv("C:\our customs\code\Python\wheater analyis\WeatherData.csv")
-print(df.head())
+missing_values = ["na","n.a","n.a.","NA",0,0.0]
+df = pd.read_csv("C:\our customs\code\Python\wheater analyis\WeatherData.csv", na_values= missing_values)
+df.dropna(axis= 0, inplace= True, how="any")
+df_unvalid = df[(df["Humidity_pct"]<0)|
+                (df["Humidity_pct"]>100)|
+                (df["Temperature_C"]>60)|
+                (df["Temperature_C"]<-90)|
+                (df["Precipitation_mm"]<0)|
+                (df["Precipitation_mm"]>500)|
+                (df["Wind_Speed_kmh"]<0)|
+                (df["Wind_Speed_kmh"]>250)
+                ]
+df = df.drop(df_unvalid.index)
+print(df)
+print(f"\nSummary of statistics:\n{df.describe()}")
