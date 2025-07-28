ad66504d97e9b8b997b5071991c82d8901a00a4c
Kasra Naal <kasranaalagsp1088@gmail.com>
2025-07-28 22:55:56 +0330

    add print

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -10,7 +10,6 @@ def get_months():
 
 get_months()
 
-<<<<<<< HEAD
 def conver_to_season(month):
     if month in [12,1,2]:
         return "Winter"
@@ -23,46 +22,26 @@ def conver_to_season(month):
     
 def get_season():
     df["Season"] = df["Month"].apply(conver_to_season)
-    # print(df)
+    print(df)
 
     print (print(df["Month"].value_counts().sort_index()))    
     print (print(df["Season"].value_counts())
 )
-=======
-
-def convert_to_season(month):
-    if month in [12, 1, 2]:
-        return "Winter"
-    elif month in [3, 4, 5]:
-        return "Spring"
-    elif month in [6, 7, 8]:
-        return "Summer"
-    elif month in [9, 10, 11]:
-        return "Fall"
-
-
-def get_season():
-    df["Season"] = df["Month"].apply(convert_to_season)
-    print(df)
->>>>>>> d48fbb77c0f3b52ab2b98a9a35386df31bd5fef3
-
 
 get_season()
 
-<<<<<<< HEAD
-# def month_season_temprature_statistics():
-#     temperature_mean = df.groupby(["Month", "Season"])["Temperature_C"].mean().reset_index()
-#     temperature_max = df["Temperature_C"].max()
-#     temperature_min = df["Temperature_C"].min()
-#     return temperature_mean
+def month_season_temprature_statistics():
+    temperature_mean = df.groupby(["Month", "Season"])["Temperature_C"].mean().reset_index()
+    temperature_max = df["Temperature_C"].max()
+    temperature_min = df["Temperature_C"].min()
+    return temperature_mean
 
-# print(month_season_temprature_statistics())
+print(month_season_temprature_statistics())
 
-# def summry_statistic():
-#     print(f"\nSummary of statistics:\n{df.drop(columns= ["Month"]).describe()}")
+def summry_statistic():
+    print(f"\nSummary of statistics:\n{df.drop(columns= ["Month"]).describe()}")
 
-# summry_statistic()
-=======
+summry_statistic()
 
 def summry_statistic():
     df.drop(columns= ["Month"], inplace = True)
@@ -89,4 +68,3 @@ def get_date():
 
 
 get_date()
->>>>>>> d48fbb77c0f3b52ab2b98a9a35386df31bd5fef3
