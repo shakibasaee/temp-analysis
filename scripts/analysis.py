1388961f325ddfd0cded10317fd54a0c404843e0
shakibasaee <152013662+shakibasaee@users.noreply.github.com>
2025-08-01 10:18:17 +0330

    Add monthly temperature plot

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -1,48 +1,50 @@
 from matplotlib import pyplot as plt
 import pandas as pd
+# from
+
 
 def get_months(df):
-    df["Month"] = pd.to_datetime(df["Date_Time"]).dt.month  # convert "Date_Time" to datetime and add to "Month" series
+    df["Month"] = pd.to_datetime(
+        df["Date_Time"]
+    ).dt.month  # convert "Date_Time" to datetime and add to "Month" series
     return df
 
 
 def convert_to_season(month):
-    if month in [12,1,2]:
+    if month in [12, 1, 2]:
         return "Winter"
-    elif month in [3,4,5]:
+    elif month in [3, 4, 5]:
         return "Spring"
-    elif month in [6,7,8]:
+    elif month in [6, 7, 8]:
         return "Summer"
-    elif month in [9,10,11]:
+    elif month in [9, 10, 11]:
         return "Autumn"
     else:
         return "Unknown"
 
 
 def month_temp_statistics(df):
-    month_temp_statistic_df = df.groupby("Month")["Temperature_C"].agg(
-        Mean_temp_c = "mean",
-        Min_temp_c = "min",
-        Max_temp_c = "max"
-    ).reset_index()
-    
+    month_temp_statistic_df = (
+        df.groupby("Month")["Temperature_C"]
+        .agg(Mean_temp_c="mean", Min_temp_c="min", Max_temp_c="max")
+        .reset_index()
+    )
+
     return month_temp_statistic_df
 
 
 def season_temp_statistic(df):
-    season_temp_statistic_df = df.groupby("Season")["Temperature_C"].agg(
-        Mean_temp_C = "mean",
-        Max_temp_C = "max",
-        Min_temp_C = "min"
-    ).reset_index()
+    season_temp_statistic_df = (
+        df.groupby("Season")["Temperature_C"]
+        .agg(Mean_temp_C="mean", Max_temp_C="max", Min_temp_C="min")
+        .reset_index()
+    )
     return season_temp_statistic_df
 
 
 def summry_statistic(df):
-    df.drop(columns= ["Month"], inplace = True)
+    df.drop(columns=["Month"], inplace=True)
     print(f"\nSummary of statistics:\n{df.describe()}")
-
-
 
 
 def get_date(df):
@@ -57,5 +59,20 @@ def get_date(df):
     plt.grid(True)
     plt.style.use("ggplot")
     plt.xticks(fontsize=7)
+
+    plt.show()
+
+
+def months_plot(df):
+    stats = month_temp_statistics(df)
+    stats.set_index("Month", inplace=True)
+
+    stats["Mean_temp_c"].plot(kind="bar", figsize=(10, 6), color="red")
+    plt.style.use("ggplot")
+    plt.title("Average Monthly Temperature")
+    plt.xlabel("Month")
+    plt.ylabel("Mean Temperature (°C)")
+    plt.grid(True)
+    plt.style.use("ggplot")
 
     plt.show()
