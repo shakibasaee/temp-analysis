4d151862bde2999bde6122a89c529f7ebd4f928c
Kasra Naal <kasranaalagsp1088@gmail.com>
2025-08-11 23:22:38 +0330

    fix open argument

--- scripts/processing_data/load_data.py
+++ scripts/processing_data/load_data.py
@@ -5,7 +5,7 @@ def load_data(file_path, delimiter=",", encoding = "utf-8"):
     if isinstance (file_path, str):
         if not os.path.exists(file_path):
             raise FileNotFoundError (f"{file_path} not found!")
-        file_obj = open(file_path, mode="r", encoding="utf-8")
+        file_obj = open(file_path, mode="r", encoding=encoding)
         df = pd.read_csv(file_obj, delimiter= delimiter)
 
     else:
