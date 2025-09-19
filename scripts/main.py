4a11ca74fb3a9083fefcb37f28b8b2a8c46ebf10
Kasra Naal <kasranaalagsp1088@gmail.com>
2025-09-20 01:04:35 +0330

    restor changes on commit 0bf797c8b7ef3a2cc20ca31ab77b5a1cb9c1138f

--- scripts/main.py
+++ scripts/main.py
@@ -13,14 +13,6 @@ from analysis import (
     month_temp_statistics,
     season_temp_statistic,
     summry_statistic,
-    regression_alg
-)
-from processing_data.data_cleaning import (
-    filter_data,
-    validate_data,
-    simplify_data,
-    save_data,
-    filter_by_date
 )
 from visualization.plots import (
     months_plot,
@@ -28,10 +20,6 @@ from visualization.plots import (
     get_date,
     save_all_plots,
 )
-from processing_data.load_data import (
-    load_data
-)
-import pandas as pd
 
 
 def get_user_inputs():
@@ -63,57 +51,17 @@ def main():
 
     df = get_months(df)
     df["Season"] = df["Month"].apply(convert_to_season)
-    print(f"Weather data is:\n{df}")
 
-    month_temp_statistic_df = month_temp_statistics(df)
-    print(f"Statistics based on month:\n{month_temp_statistic_df}")
-    season_temp_statistic_df = season_temp_statistic(df)
-    print(f"Statistics based on season:\n{season_temp_statistic_df}")
+    print("Weather data:\n", df)
+    print("Monthly statistics:\n", month_temp_statistics(df))
+    print("Seasonal statistics:\n", season_temp_statistic(df))
+    print("Summary:\n", summry_statistic(df))
 
-    df_summry= summry_statistic(df)
-    print (df_summry)
     months_plot(df)
     season_plot(df)
     get_date(df)
     save_all_plots(df)
 
-
-    model, model_columns = regression_alg(df)
-    start_day = input("Enter date:\n")
-    future_dates = pd.date_range(start_day, periods=1)
-    pred_df = pd.DataFrame(
-        {
-            "Date_Time" : future_dates,
-            "City" : ["Sanandaj"],
-        }
-    )
-
-    pred_df["Date_Time"] = pd.to_datetime(pred_df["Date_Time"])
-    pred_df["Day_of_year"] = pred_df["Date_Time"].dt.dayofyear
-    pred_df["Year"] = pred_df["Date_Time"].dt.year
-    pred_df = pd.get_dummies(pred_df, columns=["City"])
-    cols_to_int = [col for col in pred_df.columns if col not in ["Date_Time"]]
-    pred_df[cols_to_int] = pred_df[cols_to_int].astype(int)
-
-    date_col = pred_df["Date_Time"].copy()
-
-    for col in model_columns:
-        if col not in pred_df.columns:
-            pred_df[col] = 0
-
-    pred_df = pred_df[model_columns]
-
-    y_future_pred = model.predict(pred_df)
-
-    result = pd.DataFrame(
-        {
-            "Date": date_col,
-            "City": "Sanandaj",
-            "Predicted_Temprature" : y_future_pred
-        }
-    )
-
-    print (result)
 
 if __name__ == "__main__":
     main()
