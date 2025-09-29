3358a72e5b619a67c0ad2961257eb00fbaeccc0f
shakibasaee <152013662+shakibasaee@users.noreply.github.com>
2025-09-29 19:34:59 +0330

    style: reformat code for consistency

--- scripts/app.py
+++ scripts/app.py
@@ -3,22 +3,25 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import datetime as dt
 from analysis import (
-    months_plot, 
-    season_plot, 
-    get_date, 
-    get_months, 
+    months_plot,
+    season_plot,
+    get_date,
+    get_months,
     convert_to_season,
-    regression_alg, 
-    reg_plot)
+    regression_alg,
+    reg_plot,
+)
 from processing_data.data_cleaning import filter_by_date
-from processing_data.load_data import load_data
+
+# from processing_data.load_data import load_data
 from processing_data.regression_runner import reg_runner
+
 
 def streamlit():
     st.title("Weather Data Analysis")
     st.write("This app analyzes weather data.")
 
-    df = load_data()
+    df = pd.read_csv("..\\temp-analysis\\data\\new_weather_data.csv")
     df["Date_Time"] = pd.to_datetime(df["Date_Time"])
 
     min_date = df["Date_Time"].min()
@@ -32,7 +35,10 @@ def streamlit():
     )
 
     if len(date_range) == 2:
-        start_date, end_date = pd.to_datetime(date_range[0]), pd.to_datetime(date_range[1])
+        start_date, end_date = (
+            pd.to_datetime(date_range[0]),
+            pd.to_datetime(date_range[1]),
+        )
         filtered_df = filter_by_date(df, start_date, end_date)
         filtered_df = get_months(filtered_df)
 
@@ -51,13 +57,13 @@ def streamlit():
 
         st.subheader("Prediction section")
         pre_date = st.date_input(
-            "Select date to predect", 
-            value=(pd.to_datetime(date_range[1]) + dt.timedelta(days = 1)).date()
+            "Select date to predect",
+            value=(pd.to_datetime(date_range[1]) + dt.timedelta(days=1)).date(),
         )
 
         cities = ["Sanandaj", "Mashhad", "Yazd", "Bandar Abbas", "Rasht"]
         city = st.selectbox("Select city", cities)
-        
+
         st.subheader("Temprature prediction")
         result_pred_df = reg_runner(regression_alg, df, city, pre_date)
         fig = reg_plot(filtered_df, result_pred_df, city)
