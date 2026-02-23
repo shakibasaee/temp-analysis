e2c5c4d95fcbcd0c8655930deda8d4a9b9abe5dc
Kasra Naal <kasranaalagsp1088@gmail.com>
2026-02-23 20:05:45 +0330

    create input model

--- scripts/api/main-api.py
+++ scripts/api/main-api.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"massage": "ML API is running"}
+
+class PredictionInput(BaseModel):
+    lag1: float
+    lag2: float
+    lag3: float
+    
