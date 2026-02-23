3d544dcb4cd7793cfcbf7580489453f39bf0a9a7
Kasra Naal <kasranaalagsp1088@gmail.com>
2026-02-23 20:38:03 +0330

    add preditct endpoint and test with test data

--- scripts/api/main-api.py
+++ scripts/api/main-api.py
@@ -11,4 +11,10 @@ class PredictionInput(BaseModel):
     lag1: float
     lag2: float
     lag3: float
-    
+
+
+@app.post("/predict")
+def predict_temprature(data: PredictionInput):
+    predicted_value = (data.lag1 + data.lag2 + data.lag3) / 3
+
+    return {"predection_temprature": predicted_value}
