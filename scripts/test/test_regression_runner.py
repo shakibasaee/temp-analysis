86c10ff336d4dce0360e2e5b3b56b460d720f708
Kasra Naal <kasranaalagsp1088@gmail.com>
2026-02-26 00:40:57 +0330

    add test for output structure in reg_tunner

---
+++ scripts/test/test_regression_runner.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import pytest
+from processing_data.regression_runner import reg_runner
+
+class DummyModel:
+
+    def predict (self, x):
+        return [25]
+
+def dummy_regression(df):
+    return DummyModel(), ["Day_of_yer", "Year", "City"]
+
+def test_regression_runner_output_structure():
+    df = pd.DataFrame()
+    result = reg_runner(
+        regression_alg = dummy_regression,
+        df = df,
+        city = "Tehran",
+        start_day = "2024-01-01"
+    )
+
+    assert isinstance(result , pd.DataFrame)
+    assert "Date" in result.columns
+    assert "City" in result.columns
+    assert "Predicted_Temprature" in result.columns
+    assert result.iloc[0]["City"] == "Tehran"
