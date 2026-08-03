00c04bf26d655d955724ba35876d7a62548a0370
Kasra Naal <kasranaalagsp1088@gmail.com>
2026-08-03 13:26:52 +0330

    isolate regression renner tests

--- scripts/test/test_regression_runner.py
+++ scripts/test/test_regression_runner.py
@@ -1,54 +1,66 @@
 import pandas as pd
+
 from processing_data.regression_runner import reg_runner
 
-class DummyModelOutput:
 
-    def predict (self, x):
+class DummyOutputModel:
+    def predict(self, features):
         return [25]
 
-def dummy_regression(df):
-    return DummyModelOutput(), ["Day_of_yer", "Year", "City"]
 
-def test_regression_runner_output_structure():
-    df = pd.DataFrame()
-    result = reg_runner(
-        regression_alg = dummy_regression,
-        df = df,
-        cities = "Tehran",
-        start_day = "2024-01-01"
-    )
-
-    assert isinstance(result , pd.DataFrame)
-    assert "Date" in result.columns
-    assert "City" in result.columns
-    assert "Predicted_Temprature" in result.columns
-    assert result.iloc[0]["City"] == "Tehran"
-
-
-class DummyModelPredict:
+class PredictSpyModel:
     def __init__(self):
         self.called = False
 
-    def predict(self, x):
+    def predict(self, features):
         self.called = True
         return [30]
-    
 
 
-def dummy_regression(df):
-    model = DummyModelPredict()
-    return model, ["Day_of_year", "Year", "City"]
+def output_regression(_df):
+    model_columns = [
+        "Day_of_year",
+        "Year",
+        "City_Tehran",
+    ]
+
+    return DummyOutputModel(), model_columns
 
 
-def test_predict_called():
-    df = pd.DataFrame()
-    model, cols = dummy_regression(df)
-
+def test_regression_runner_returns_expected_output_structure():
     result = reg_runner(
-        regression_alg=lambda df: (model, cols),
-        df=df,
-        cities="Tehran",
-        start_day="2024-01-01"
-    )
+    output_regression,
+    pd.DataFrame(),
+    "Tehran",
+    "2024-01-01",
+)
+
+    assert list(result.columns) == [
+        "Date",
+        "City",
+        "Predicted_Temprature",
+    ]
+    assert result.iloc[0]["Date"] == pd.Timestamp("2024-01-01")
+    assert result.iloc[0]["City"] == "Tehran"
+    assert result.iloc[0]["Predicted_Temprature"] == 25
+
+
+def test_regression_runner_calls_model_predict():
+    model = PredictSpyModel()
+    model_columns = [
+        "Day_of_year",
+        "Year",
+        "City_Tehran",
+    ]
+
+    def regression_stub(_df):
+        return model, model_columns
+
+    reg_runner(
+    regression_stub,
+    pd.DataFrame(),
+    "Tehran",
+    "2024-01-01",
+)
 
     assert model.called is True
